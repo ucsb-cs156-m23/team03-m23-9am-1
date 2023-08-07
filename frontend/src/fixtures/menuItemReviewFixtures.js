043b31db28727c5c9b9e615bec78cd02be85d21e
Pablo <pds@ucsb.edu>
2023-08-07 13:23:16 -0700

    actual implementation of fixtures

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,36 @@
+const menuItemReviewFixtures = {
+    oneReview: {
+        "itemId": 1,
+        "reviewerEmail": "pds@ucsb.edu",
+        "stars": "4",
+        "dateReviewed": "2022-01-02T12:00:00",
+        "comments": "The pasta was good but a little salty"
+    },
+    threeReviews: [
+        {
+            "itemId": 1,
+            "reviewerEmail": "alecmorrison@ucsb.edu",
+            "stars": "3",
+            "dateReviewed": "2022-01-02T12:00:00",
+            "comments": "A little bland"
+        },
+        {
+            "itemId": 2,
+            "reviewerEmail": "adhit@ucsb.edu",
+            "stars": "5",
+            "dateReviewed": "2022-01-02T12:00:00",
+            "comments": "cooked perfectly"
+            
+        },
+        {
+            "itemId": 3,
+            "reviewerEmail": "aasish@ucsb.edu",
+            "stars": "2",
+            "dateReviewed": "2022-01-02T12:00:00",
+            "comments": "dont order the chicken"
+        }
+    ]
+};
+
+
+export { menuItemReviewFixtures };
